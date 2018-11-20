a604b2681157c2f69b9e2b850636cd8ee1d8a718
kyleesantos <ksantos@latedays.andrew.cmu.edu>
2018-11-19 21:41:23 -0500

    Change number of iterations

--- code/openMP/regression.h
+++ code/openMP/regression.h
@@ -5,12 +5,13 @@ typedef struct {
 
 typedef struct {
   float num;
-  char padding[124];
+  char padding[60];
 } num_t;
 
-const float STEP_SIZE_BATCH = 0.00005;
-const int NUM_ITER_BATCH =  1000000;
+const float STEP_SIZE_BATCH = 0.0001;
+const int NUM_ITER_BATCH =  1000;
 const float STEP_SIZE_STOCH = 0.0001;
-const int NUM_ITER_STOCH =  500000;
+const int NUM_ITER_STOCH =  1000;
+const int BATCH_SIZE_STOCH = 5;
 const float INIT_B0 = 0.0;
 const float INIT_B1 = 0.0;
