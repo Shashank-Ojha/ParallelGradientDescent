3ee315b59241611f712d8c705910985e438f3d4b
Shashank Ojha <shashano@andrew.cmu.edu>
2018-11-27 22:06:22 -0500

    added approx for baseline code

--- code/openMP/gd.h
+++ code/openMP/gd.h
@@ -6,6 +6,7 @@ typedef struct {
 const float STEP_SIZE_BATCH = 0.0000001;
 const int NUM_ITER_BATCH =  100000;
 
-const float STEP_SIZE_STOCH = 0.0001;
+const float STEP_SIZE_STOCH = 0.01;
 const int NUM_ITER_STOCH =  100000;
 const int BATCH_SIZE_STOCH = 5;
+const int ITER_LIMIT = 10000000;
