4f7f59c5e96561527a0d4053ffc9ce01e1fb55c9
Kylee T Santos <ksantos@andrew.cmu.edu>
2018-12-07 00:42:03 -0500

    Add timing inside CUDA kernel

--- code/cuda/main.cpp
+++ code/cuda/main.cpp
@@ -61,40 +61,51 @@ int checkInputArguments(char* filename, float alpha, int blocks,
 }
 
 float evaluate(estimate_t* estimate, float x){
-  return (estimate->b1)*x;
+  return (estimate->b1)*x + estimate->b0;
 }
 
-float getdB1(float x, float y, estimate_t* estimate){
+float getdB0(float x, float y, estimate_t* estimate, int N){
   float prediction = evaluate(estimate, x);
-  return -2.0 * (y-prediction)*x;
+  return (-2.0 / static_cast<float>(N)) * (y-prediction);
+}
+
+float getdB1(float x, float y, estimate_t* estimate, int N){
+  float prediction = evaluate(estimate, x);
+  return (-2.0 / static_cast<float>(N)) * (y-prediction) * x;
 }
 
 estimate_t* bgd(int N, float* x, float* y){
   estimate_t* estimate = (estimate_t*)malloc(sizeof(estimate_t));
+  estimate -> b0 = INIT_B0;
   estimate -> b1 = INIT_B1;
 
   for(int i = 0; i < NUM_ITER_BATCH; i++){
-    float db1 = 0;
-    for(int j = 0; j < N; j++)
-    {
-      db1 += (1.0 / static_cast<float>(N)) * getdB1(x[j], y[j], estimate);
+    float db0 = 0.0;
+    float db1 = 0.0;
+    for(int j = 0; j < N; j++) {
+      db0 += getdB0(x[j], y[j], estimate, N);
+      db1 += getdB1(x[j], y[j], estimate, N);
     }
 
-    estimate -> b1 = (estimate -> b1) - (STEP_SIZE_BATCH * db1);
+    estimate -> b0 -= STEP_SIZE_BATCH * db0;
+    estimate -> b1 -= STEP_SIZE_BATCH * db1;
   }
+
   return estimate;
 }
 
 estimate_t* sgd(int N, float* x, float* y){
   estimate_t* estimate = (estimate_t*)malloc(sizeof(estimate_t));
+  estimate -> b0 = INIT_B0;
   estimate -> b1 = INIT_B1;
   for(int i = 0; i < NUM_ITER_STOCH; i++){
     //pick a point randomly
     int pi = rand() % N;
 
-    float db1 = (1.0 / static_cast<float>(N)) * getdB1(x[pi], y[pi], estimate);
-
-    estimate -> b1 = (estimate -> b1) - (STEP_SIZE_STOCH * db1);
+    float db0 = getdB0(x[pi], y[pi], estimate, N);
+    float db1 = getdB1(x[pi], y[pi], estimate, N);
+    estimate -> b0 -= STEP_SIZE_STOCH * db0;
+    estimate -> b1 -= STEP_SIZE_STOCH * db1;
   }
 
   return estimate;
@@ -173,20 +184,20 @@ int main(int argc, char** argv)
     srand(418);
 
     estimate_t* estimate_bgd = bgd(N, x, y);
-    printf("Batch: y = %.2f (x)\n", estimate_bgd -> b1);
+    printf("Batch: y = %.2f (x) + %.2f\n", estimate_bgd -> b1, estimate_bgd -> b0);
 
     estimate_t* estimate_sgd = sgd(N, x, y);
-    printf("Stochastic: y = %.2f (x)\n", estimate_sgd -> b1);
+    printf("Stochastic: y = %.2f (x) + %.2f\n", estimate_sgd -> b1, estimate_sgd -> b0);
 
-    estimate_t* estimate_sgdCuda = sgdCuda(N, x, y, alpha, refSlope,
+    estimate_t* estimate_sgdCuda = sgdCuda(N, x, y, alpha, refMSE,
                                            blocks, threadsPerBlock);
 
-    printf("Cuda Stochastic: y = %.2f (x)\n", estimate_sgdCuda -> b1);
+    printf("Cuda Stochastic: y = %.2f (x) + %.2f\n", estimate_sgdCuda -> b1, estimate_sgdCuda -> b0);
 
     estimate_t* estimate_sgdCudaByBlock = sgdCudaByBlock(N, x, y, alpha,
-                        refSlope, samplesPerThread, blocks, threadsPerBlock);
+                        refMSE, samplesPerThread, blocks, threadsPerBlock);
 
-    printf("Cuda Stochastic by block: y = %.2f (x)\n", estimate_sgdCudaByBlock -> b1);
+    printf("Cuda Stochastic by block: y = %.2f (x) + %.2f\n", estimate_sgdCudaByBlock -> b1, estimate_sgdCudaByBlock -> b0);
 
     return 0;
 }
