e356746c0ce8ff38ab25a82cff4fed89d279d02a
Kylee T Santos <ksantos@andrew.cmu.edu>
2018-12-06 22:02:55 -0500

    Implement SGD per block design and make new data

--- code/cuda/gd.cu
+++ code/cuda/gd.cu
@@ -6,6 +6,8 @@
 
 #include "regression.h"
 
+#define THREADS_PER_BLOCK 16
+
 void printCudaInfo()
 {
     // for fun, just print out some stats on the machine
@@ -84,15 +86,13 @@ estimate_t* sgdCuda(int N, float* x, float* y, float alpha, float opt,
   cudaMalloc((void **)&device_Y, sizeof(float) * N);
   cudaMalloc((void **)&device_estimates, sizeof(estimate_t) * totalThreads);
 
-  //might have to check this
   estimate_t* estimates = (estimate_t*)calloc(totalThreads, sizeof(estimate_t));
+  for (int i = 0; i < totalThreads; i++) {
+      estimates[i].b1 = INIT_B1;
+  }
 
-  cudaMemcpy(device_X, x, N * sizeof(float),
-             cudaMemcpyHostToDevice);
-
-  cudaMemcpy(device_Y, y, N * sizeof(float),
-             cudaMemcpyHostToDevice);
-
+  cudaMemcpy(device_X, x, N * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(device_Y, y, N * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(device_estimates, estimates, totalThreads * sizeof(estimate_t),
              cudaMemcpyHostToDevice);
 
@@ -112,11 +112,124 @@ estimate_t* sgdCuda(int N, float* x, float* y, float alpha, float opt,
                cudaMemcpyDeviceToHost);
 
     ret -> b1 = 0.0;
-    for(int j = 0; j < totalThreads; j++) {
+    int flag = 0;
+    for(int j = 0; j < blocks; j++) {
       ret -> b1 += estimates[j].b1;
+      if ((lower < estimates[j].b1) && (estimates[j].b1 < upper)) {
+          ret -> b1 = estimates[j].b1;
+          flag = 1;
+          break;
+      }
     }
+    if (flag) break;
 
     ret -> b1 /= static_cast<float>(totalThreads);
+
+    if(num_steps > ITER_LIMIT || (lower < (ret -> b1) && (ret -> b1) < upper))
+      break;
+
+    num_steps += 1;
+  }
+
+  printf("Num iterations: %d\n", num_steps);
+
+  return ret;
+}
+
+//-----------------------------------------------------------------------------
+
+// Assumes the number of indexes is equal to N
+__global__ void
+sgdStepByBlock(int N, float* device_X, float* device_Y,
+               estimate_t* device_estimates, int k, curandState* states) {
+
+  __shared__ float thread_db1[THREADS_PER_BLOCK];
+
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+  thread_db1[threadIdx.x] = 0.0;
+  curandState localState = states[index];
+
+  for(int i = 0; i < k; i++)
+  {
+      int pi = curand(&localState) % N;
+      thread_db1[threadIdx.x] += (1.0 / static_cast<float>(N)) *
+            getdB1Cuda(device_X[pi], device_Y[pi], device_estimates+blockIdx.x);
+  }
+  thread_db1[threadIdx.x] /= static_cast<float>(k);
+  states[index] = localState;
+
+  __syncthreads();
+
+
+  if(threadIdx.x == 0)
+  {
+      float db1 = 0.0;
+      for(int i = 0; i < blockDim.x; i++){
+          db1 += thread_db1[i];
+      }
+      db1 /= static_cast<float>(blockDim.x);
+
+      device_estimates[blockIdx.x].b1 -= (STEP_SIZE_STOCH * db1);
+  }
+
+}
+
+// Running SGD on each block with all threads sampling k points and averaging result
+// after each SGD step. Checking convergence after each step
+estimate_t* sgdCudaByBlock(int N, float* x, float* y, float alpha, float opt,
+                     int k, int blocks, int threadsPerBlock){
+  curandState *states;
+  float* device_X;
+  float* device_Y;
+  estimate_t* device_estimates;
+
+  int totalThreads = blocks * threadsPerBlock;
+
+  cudaMalloc((void**)&states, totalThreads * sizeof(curandState));
+  cudaMalloc((void **)&device_X, sizeof(float) * N);
+  cudaMalloc((void **)&device_Y, sizeof(float) * N);
+  cudaMalloc((void **)&device_estimates, sizeof(estimate_t) * blocks);
+
+  estimate_t* estimates = (estimate_t*)calloc(totalThreads, sizeof(estimate_t));
+  for (int i = 0; i < totalThreads; i++) {
+      estimates[i].b1 = INIT_B1;
+  }
+
+  cudaMemcpy(device_X, x, N * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(device_Y, y, N * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(device_estimates, estimates, blocks * sizeof(estimate_t),
+             cudaMemcpyHostToDevice);
+
+  setup_kernel<<<blocks, threadsPerBlock>>>(states);
+
+  estimate_t* ret = (estimate_t*)malloc(sizeof(estimate_t));
+
+  float upper = opt + (alpha/2.0)*opt;
+  float lower = opt - (alpha/2.0)*opt;
+
+  int num_steps = 0;
+  while(true)
+  {
+    sgdStepByBlock<<<blocks, threadsPerBlock>>>(N, device_X, device_Y, device_estimates, k, states);
+
+    cudaMemcpy(estimates, device_estimates, blocks * sizeof(estimate_t),
+               cudaMemcpyDeviceToHost);
+
+    ret -> b1 = 0.0;
+    int flag = 0;
+    for(int j = 0; j < blocks; j++) {
+      ret -> b1 += estimates[j].b1;
+      if ((lower < estimates[j].b1) && (estimates[j].b1 < upper)) {
+          ret -> b1 = estimates[j].b1;
+          flag = 1;
+          break;
+      }
+    }
+
+    if (flag) break;
+
+    ret -> b1 /= static_cast<float>(blocks);
 
     if(num_steps > ITER_LIMIT || (lower < (ret -> b1) && (ret -> b1) < upper))
       break;
--- code/generate_data.py
+++ code/generate_data.py
@@ -1,35 +1,38 @@
 import numpy as np
 import argparse
+import os
 
 def get_args():
     parser = argparse.ArgumentParser(description='Read number of data points')
     parser.add_argument('-n', help='Number of data points')
     parser.add_argument('-m', help='Slope of best fit line')
     parser.add_argument('-b', help='Intercept of best fit line')
+    parser.add_argument('-std', help='Standard dev of best fit line')
+    parser.add_argument('-r', help='Range of x values for data')
     args = parser.parse_args()
     return args
 
-def generateNoise(numPoints):
-    return np.random.normal(0, 10, 2*numPoints)
+def generateNoise(numPoints, std):
+    return np.random.normal(0, std, numPoints)
 
-def generatePoints(numPoints):
-    return np.random.uniform(0, 1000, numPoints)
+def generatePoints(numPoints, r):
+    return np.random.uniform(0, r, numPoints)
 
 def getY(x, m, b):
     return m * x + b
 
-def generateData(numPoints, m, b):
-    noise = generateNoise(numPoints)
-    points = generatePoints(numPoints)
+def generateData(numPoints, m, b, std, r):
+    noise = generateNoise(numPoints, std)
+    points = generatePoints(numPoints, r)
     x = []
     y = []
     for i in range(numPoints):
-        x += [points[i] + noise[i]]
-        y += [getY(points[i], m, b) + noise[i+1]]
+        x += [points[i]]
+        y += [getY(points[i], m, b) + noise[i]]
     return x,y
 
-def generateString(numPoints, m, b):
-    x,y = generateData(numPoints, m, b)
+def generateString(numPoints, m, b, std, r):
+    x,y = generateData(numPoints, m, b, std, r)
     ret = '%d\n' % numPoints
     for i in range(numPoints):
         ret += '%f\t%f\n' % (x[i], y[i])
@@ -44,8 +47,13 @@ def main():
     numPoints = int(args.n)
     slope = float(args.m)
     intercept = float(args.b)
-    output = '%d.txt' % numPoints
-    res = generateString(numPoints, slope, intercept)
+    std = float(args.std)
+    r = int(args.r)
+    path = 'data/%.2f' % slope
+    if not os.path.exists(path):
+        os.makedirs(path)
+    output = '%s/%d_%.2f_%d.txt' % (path, numPoints, std, r)
+    res = generateString(numPoints, slope, intercept, std, r)
     writeToFile(output, res)
 
 
